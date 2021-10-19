3f7cb1dfbfbf7005f73f10daf85e4bc2c15ce52e
sirogane1013 <stlouis.route66@gmail.com>
2021-10-19 11:32:09 +0900

    Add basic feature
    コマンドの送信者のみ見えるプライベートチャンネルの作成

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 
+import discord
 from discord.ext import commands
+from discord.utils import get
 from dotenv import load_dotenv
+
+import settings
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -10,13 +14,22 @@ bot = commands.Bot(command_prefix='/')
 
 @bot.event
 async def on_ready():
-    print('Logged in')
+    print("Logged in")
 
 
-# write your commands here
-# @bot.command()
-# async def my_command(ctx):
-#     await ctx.send("hello")
+@bot.command()
+async def mkch(ctx):
+    prefix = settings.CHANNEL_PREFIX
+    guild = ctx.guild
+    author = ctx.author
+    author_name = author.display_name
+    channel = await guild.create_text_channel(
+        prefix + author_name,
+        overwrites={
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            author: discord.PermissionOverwrite(read_messages=True),
+        })
+    await ctx.send(f"Created {channel.mention}")
 
 
 bot.run(TOKEN)
